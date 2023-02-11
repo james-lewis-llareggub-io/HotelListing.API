5b9134358f3fc77aece6efe3dd7eac205ee9fa1d
James Lewis <james.lewis@llareggub.io>
2023-02-11 13:11:04 +0000

    refactor `GetCountryDetailDTO` to inherit from `GetCountryDTO` data transfer object (to reduce duplication)

--- HotelListing.API/Models/Country/GetCountryDetailDTO.cs
+++ HotelListing.API/Models/Country/GetCountryDetailDTO.cs
@@ -1,12 +1,6 @@
 ﻿namespace HotelListing.API.Models.Country;
 
-public class GetCountryDetailDTO
+public class GetCountryDetailDTO : GetCountryDTO
 {
-    public int Id { get; set; }
-    
-    public string Name { get; set; }
-
-    public string ShortName { get; set; }
-    
     public IList<GetHotelDTO> Hotels { get; set; }
 }
