14e00ff88e29cc976d9c455a713ea7142b2ae36f
James Lewis <james.lewis@llareggub.io>
2023-02-13 19:34:55 +0000

    add identity user roles to db context for ADMIN and USER by implementing `IEntityTypeConfiguration` and refactor existing seeded data (lookups) e.g. countries and hotels

---
+++ HotelListing.API/Configurations/Database/RoleConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HotelListing.API.Configurations.Database;
+
+public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole>
+{
+    public void Configure(EntityTypeBuilder<IdentityRole> builder)
+    {
+        builder.HasData(
+            new IdentityRole
+            {
+                Name = "Admin",
+                NormalizedName = "ADMIN"
+            },
+            new IdentityRole
+            {
+                Name = "User",
+                NormalizedName = "USER"
+            }
+        );
+    }
+}
---
+++ HotelListing.API/Migrations/20230213193127_AddIdentityUserRoles.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace HotelListing.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIdentityUserRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "23b39120-a3a3-44d8-8bad-e1db7d97a232", "2918898f-af65-4173-a7ba-813ebc89a83a", "User", "USER" },
+                    { "573fafc5-2107-43f7-888e-3a94d6de181e", "6ca3796f-3f39-4454-ad53-7290eaceb244", "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "23b39120-a3a3-44d8-8bad-e1db7d97a232");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "573fafc5-2107-43f7-888e-3a94d6de181e");
+        }
+    }
+}
